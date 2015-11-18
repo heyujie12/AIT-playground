d04679c17fd187312f26ec27e42eacc3fa769c05
RonanCassidy <rcassidydev@gmail.com>
2015-11-18 19:08:46 +0000

    Completed the class report included in this is a pie chart and a new page for ever student also added in different fonts and text size to fill the pages. Had many many problems with jFree with supplied the pie chart but is working now

--- StudentReportGenerator/src/main/java/org/ait/msc/project/ReportGenerator.java
+++ StudentReportGenerator/src/main/java/org/ait/msc/project/ReportGenerator.java
@@ -5,6 +5,7 @@ import java.awt.geom.Rectangle2D;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
@@ -38,52 +39,59 @@ public class ReportGenerator {
 	void createClassReport(ArrayList<StudentReport> classReport) throws SQLException, IOException
 	{	
 		try {
-			 
-			Document document = new Document();
+			
+			Font font1 = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
+	        Font font2 = new Font(Font.FontFamily.TIMES_ROMAN, 16,Font.ITALIC);
+	        Font font3 = new Font(Font.FontFamily.TIMES_ROMAN, 27);
+			Document document = new Document(PageSize.A4,50,50,PageSize.A4.getHeight()/2,50);
 			Rectangle rect = new Rectangle(PageSize.LETTER);
 			document.setPageSize(rect);
 			DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 			Calendar calendar = Calendar.getInstance();
-			String pathname = "C:\\Users\\Patrick Hall\\ReportClass"+ df.format(calendar.getTime()) + ".pdf";
+			String pathname = "C:\\Users\\Patrick Hall\\ClassReport"+ df.format(calendar.getTime()) + ".pdf";
 			File file = new File(pathname);
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
 			document.open();
 			
 			PdfContentByte cb = writer.getDirectContent();
 			float width = PageSize.A4.getWidth();
-			float height = PageSize.A4.getHeight()/2;
+			float height = PageSize.A4.getHeight()/2-50;
 			
 			PdfTemplate pie = cb.createTemplate(width, height);
 			Graphics2D g2d1 = new PdfGraphics2D(pie, width, height);
 			Rectangle2D r2d1 = new Rectangle2D.Double(0,0,width,height);
 			
-			
-			for(int i = 0; i < classReport.size(); i++)
-			{
-				Paragraph paragraphHeading = new Paragraph();
-				Paragraph paragraphResults = new Paragraph();
-				paragraphHeading.setAlignment(Element.ALIGN_CENTER);
-				paragraphHeading.add("Student: " + classReport.get(i).getStudentName() + "\n");
-				paragraphHeading.add("Parent: " + classReport.get(i).getParentName());
-				paragraphHeading.add("Class: " + classReport.get(i).getStudentClass());
-				document.add(paragraphHeading);
-				System.out.println(classReport.get(i).getStudentName());
-				paragraphResults.setAlignment(Element.ALIGN_LEFT);
-				paragraphResults.add("Overall Progress:" + classReport.get(i).getOverallProgress() + "\n");
-				System.out.println(classReport.get(i).getOverallProgress());
-				paragraphResults.add(createFirstTable(classReport.get(i).getAdditionProficiency(), 
-						classReport.get(i).getSubtractionProficiency(),classReport.get(i).getMultipicationProficiency(), classReport.get(i).getDivisionProficiency() ));
-				paragraphResults.add("\nTables " + classReport.get(i).getStudentName() + " is good in:\n");
-				paragraphResults.add(classReport.get(i).getStrongTables() + "\n\n");
-				paragraphResults.add("Tables " + classReport.get(i).getStudentName() + " could improve in:\n");
-				paragraphResults.add(classReport.get(i).getWeakTables());
-				document.add(paragraphResults);
-				
-			}
-			
 			getPieChart(classReport).draw(g2d1, r2d1);
 			g2d1.dispose();
 			cb.addTemplate(pie,0,height);
+			for(int i = 0; i < classReport.size(); i++)
+			{
+				Paragraph paragraphHeading = new Paragraph(25);
+				Paragraph paragraphResults = new Paragraph(15);
+				paragraphHeading.setAlignment(Element.ALIGN_CENTER);
+				paragraphHeading.setFont(font1);
+				paragraphHeading.add(" Student: " + classReport.get(i).getStudentName() + "\n");
+				paragraphHeading.add(" Parent: " + classReport.get(i).getParentName() + "\n");
+				paragraphHeading.add(" Class: " + classReport.get(i).getStudentClass()+ "\n");
+				paragraphHeading.setSpacingAfter(50);
+				document.add(paragraphHeading);
+				
+				paragraphResults.setAlignment(Element.ALIGN_LEFT);
+				paragraphResults.setFont(font2);
+				paragraphResults.add("Overall Progress: " + classReport.get(i).getOverallProgress() + "\n");
+				
+				paragraphResults.add("\nTables " + classReport.get(i).getStudentName() + " is good in: ");
+				paragraphResults.add(classReport.get(i).getStrongTables() + "\n\n");
+				paragraphResults.add("Tables " + classReport.get(i).getStudentName() + " could improve in: ");
+				paragraphResults.add(classReport.get(i).getWeakTables());
+				paragraphResults.add(createFirstTable(classReport.get(i).getAdditionProficiency(), 
+						classReport.get(i).getSubtractionProficiency(),classReport.get(i).getMultipicationProficiency(), classReport.get(i).getDivisionProficiency() ));
+				
+				document.add(paragraphResults);
+				document.setMargins(50,50,50,50);
+				document.newPage();
+
+			}
 			document.close();
 		}
 		catch (FileNotFoundException e) {
@@ -151,11 +159,11 @@ public class ReportGenerator {
 	    	else if(classReport.get(i).getOverallProgress().equals("bad"))
 	    		badProgress = badProgress + 20.0;
 		}
-	    dataset.setValue("Good Progress", goodProgress);
-	    dataset.setValue("Medium Progress", medProgress);
 	    dataset.setValue("Bad Progress", badProgress);
+	    dataset.setValue("Medium Progress", medProgress);
+	    dataset.setValue("Good Progress", goodProgress);
 	    
-	    return ChartFactory.createPieChart("Overall Progress", dataset,
+	    return ChartFactory.createPieChart("Overall Progress of "+classReport.get(0).getStudentClass(), dataset,
 	                true, true, false);
 	    }
     public static PdfPTable createFirstTable(String additionProficiency, String subtractionProficiency, String multipicationProficiency, String divisionProficiency ) {
